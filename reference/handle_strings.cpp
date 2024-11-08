716979c95592031b315d71df38d9218ebcaf740b
kalet Chavez <kalet.svdev@gmail.com>
2024-11-08 01:42:22 -0600

    :fire: changes in .md files and finish about handling string file

--- reference/handle_strings.cpp
+++ reference/handle_strings.cpp
@@ -1,8 +1,8 @@
 #include "loader.h"
 
 int main(){
-    //este es un texto quemado en este archivo se encuentran referencias de los metodos
-    // que se utilizan al manipular texto.
+    /* este es un texto quemado en este archivo se encuentran referencias de los metodos
+     que se utilizan al manipular texto. */
 
     string texto1 = ""; 
 
@@ -10,8 +10,8 @@ int main(){
     cout << "Ingrese lo que quisiera escribir: \n";
     cin >> texto1;
 
-    //tanto la funcion length como size sirven para lo mismo y no hay alguna diferencia en usar
-    // una u otra
+    /* tanto la funcion length como size sirven para lo mismo y no hay alguna diferencia en usar
+     una u otra */
     cout << "Usando length: " << texto1.length() << endl;
     cout << "Usando size: " << texto1.size() << endl;
 
@@ -30,8 +30,8 @@ int main(){
     cout << "Caracter( usando at ) en el indice 4: " << texto3.at(4) << "\n";
     cout << "Caracter( Usando [] ) en el indice 4: " << texto3[4] << endl;
 
-    //la diferencia entre at y "[]", es que at lanza una excepcion cuando el indice esta fuera de rango
-    // y [] no la lanza lo que puede provocar errores no controlados
+    /* la diferencia entre at y "[]", es que at lanza una excepcion cuando el indice esta fuera de rango
+     y [] no la lanza lo que puede provocar errores no controlados */
 
     //----------------------------------------------------------------------------------------------------------//
 
@@ -56,8 +56,8 @@ int main(){
 
     string texto5 = "Hola ";
     string nuevaPalabra = "";
-    short index = 0; // short es un valor entero a diferencia de int que usa 4 bytes para guardar y es un rango mayor
-                     // short solo usa 2 bytes y es un rango menor.
+    short index = 0; /* short es un valor entero a diferencia de int que usa 4 bytes para guardar y es un rango mayor
+                      short solo usa 2 bytes y es un rango menor.*/
 
     cout << "Palabra incial: " << texto5 << "\n";
 
@@ -71,5 +71,44 @@ int main(){
 
     cout << "Su nueva frase es: " << texto5 << endl;
 
+    //----------------------------------------------------------------------------------------------------------//
+
+    /* Para eliminar caracteres de un string podemos usar el metodo erase, el cual recibe como parametros lo que 
+     seria la posicion y la cantidad de carateres a eliminar desde la posicion indicada*/
+    
+    string texto6 = "Hola, buen mundo";
+
+    cout << "Frase inicial: " << texto6 << endl;
+               //posicion , cantidad de caracteres
+    texto6.erase( 6, 4);
+
+    cout << "Nueva frase: " << texto6 << endl;
+
+    //----------------------------------------------------------------------------------------------------------//
+
+    /* Para extraer una palabra o cadena de caracteres puedes usar el metodo substr, el cual recibe como parametros lo que
+    seria la posicion y la cantidad de caracteres que quieres extraer*/
+    string texto7 = "Juan no comio mucho";
+
+    cout << "Frase inicial: " << texto7 << "\n";
+
+    string extract = texto7.substr( 5, 2);
+
+    cout << "palabra extraida" << extract << endl;
+
+    //----------------------------------------------------------------------------------------------------------//
+
+    /* Para saber si una palabra existe en una cadena podemos usar el metodo find, el cual recibe lo que seria como 
+    parametro la palabra, tambien es de saber que este retorna la posicion de la primera coincidencia si no retorna
+    npos que es una constante estatica (su significado es no posicion) */
+
+    string texto8 = "Juan fue al mercado";
+    size_t posicion = texto8.find("fue");
+
+    cout << ( posicion != string::npos ) 
+    ? "fue esta en la posicion: " + posicion
+    : "No se encontro la palabra"; 
+    
+    cout << "\n";
 
 }
---
+++ shellComands.md
@@ -0,0 +1,7 @@
+# Comandos para manejo de la terminal (linux shell)
+
+## Estructura de un comando:
+
+- Las terminales o shells de linux son sensitivas a las mayusculas y minusculas( no pasa con las shells de windows y con wsl en el caso de nombre de archivos)
+por ende no es lo mismo ejecutar un comando: ```ls``` a ejecutar ```LS``` o ```Ls```, esto se debe tomar muy encuenta ya que es el principal error que se comete al uso de comandos.Los comandos tienen una estructura definida que se debe respetar para evitar errores a la hora de ejecutarse los cuales son:
+  
