ab0dbac2a0a5c963438ca266896077384328e165
kalet Chavez <kalet.svdev@gmail.com>
2024-11-10 23:27:56 -0600

    :art: create initial structure

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include "metadata.h"
 
 using namespace std;
 
 int main(){
-    cout << "Hello world !" << "\n";
+    if( isAuthenticated ){
+        cout << "Hello world !" << "\n";
+    }
+    else {
+        cout << "No puedes entrar!" << "\n";
+    }
 }
