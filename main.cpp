22230efde92a287ec11ca57395a57034526a26f1
adrianareyes99 <00243818@uca.edu.sv>
2024-11-26 21:09:57 -0600

    Agregando creditos finales

--- main.cpp
+++ main.cpp
@@ -12,20 +12,26 @@ struct usuario {
 
 // Función para cargar usuarios desde el archivo y verificar login
 bool verificarLogin(string user, string password) {  // Pasado por valor
+    // Esta libreria sirve pra esritur y lectura de archivos de texto 
     fstream archivoUsuarios;
+    // Esta variable sirve para leer las lineas de el archivo de usuarios
     string linea;
     archivoUsuarios.open("usuarios.txt", ios::in);  // Abrir archivo solo para lectura
+    // Valida si el archivo se pudo abrir o no
     if (!archivoUsuarios) {
         cout << "Error al abrir el archivo de usuarios." << endl;
         return false;
     }
 
+    // Con este bucle leemos las lineas que contiene el archivo hasta encontrar las credenciales correctas
     while (getline(archivoUsuarios, linea)) {
+        //Se usa para leer las lineas con espacios
         stringstream ss(linea);
         string archivoUser, archivoPassword;
         if (getline(ss, archivoUser, ' ') && getline(ss, archivoPassword)) {
             // Verificar si el usuario y la contraseña coinciden
             if (archivoUser == user && archivoPassword == password) {
+                // Cerramos el archivo porque ya se encontro el usuario
                 archivoUsuarios.close();
                 return true;  // Usuario y contraseña correctos
             }
@@ -37,8 +43,11 @@ bool verificarLogin(string user, string password) {  // Pasado por valor
 }
 
 // Función para registrar un nuevo usuario
+// Devuelve true si el usuario ue registrado y false si no se pudo registrar
 bool registrarUsuario(string user, string password) {  // Pasado por valor
+    // Esta libreria sirve pra esritur y lectura de archivos de texto 
     fstream archivoUsuarios;
+    // Esta variable sirve para leer las lineas de el archivo de usuarios
     string linea;
 
     // Verificar si el usuario ya existe en el archivo
@@ -80,6 +89,7 @@ int main() {
 
     cout << "Bienvenido, iniciemos la aventura" << endl;
 
+    // Ocupamos un do while para hacer un bucle hasta que el usuario digite la opcion salir
     do {
         cout << "Elige una opcion: " << endl;
         cout << "1- Ingresar" << endl;
@@ -87,6 +97,7 @@ int main() {
         cout << "3- Salir" << endl;
         cin >> opcion;
 
+        //Evaluamos la opcin que el cliente ha ingresado
         switch (opcion) {
         case 1:
             cout << "Username: " << endl;
@@ -94,7 +105,8 @@ int main() {
             cout << "Password: " << endl;
             cin >> password;
 
-            bool usuarioVerificado = verificarLogin(user, password);
+            //Devuelve true si los datos son correctos o false si son incorrectos
+            bool usuarioVerificado = verificarLogin(user, password); //mandamos a llamar a la funcion verificar login que le enviamos usuario y contrase;a
 
             if (usuarioVerificado) {
                 cout << "Usuario encontrado correctamente, bienvenido." << endl;
@@ -109,8 +121,9 @@ int main() {
             cout << "Ingrese su contraseña: " << endl;
             cin >> password;
 
+            //para ver si el usuario existe si no existe se registra 
             bool usuarioRegistrado = registrarUsuario(user, password);
-
+            
             if (usuarioRegistrado) {
                 cout << "Usuario registrado correctamente." << endl;
             } else {
@@ -127,7 +140,14 @@ int main() {
             break;
         }
 
+    // Mientras la opcion sea diferente a 3 se sigue repitiendo el menu
     } while (opcion != 3);
+    
 
+//un for no se puede utilizar por que se usa para recorrer listas o arreglos por que es algo definido que se ejecute un x cantidad 
+//do while me garantiza ejecutar el codigo almenos una vez y el while no me lo mostraria 
+
+
+// El for se usa si es una cantidad especifica de veces, el while y do while si no conoces cuantas veces, pero el do while nos permite ejecutar almenos una vez el menu
     return 0;
 }
--- screens/credenciales.cpp
+++ screens/credenciales.cpp
@@ -1,71 +1,42 @@
-#include  <ncurses.h>
+#include <ncurses.h>
 #include <unistd.h>
-
-
-char*creditosfinales[] = {
-
-    "*Kalet Adonay Chavez Alvarenga",
-    "*Mario Alexander Molina Fuentes",
-    "*Adriana Lisbeth Reyes Perez",
-    "*Carlos Alejandro Reyes Segovia",
-    "*Gladys Alejandra Rivera Iraheta",
-
-};
+#include <iostream>
 
 void mostrar_creditos(){
-printw("------------------------------------------------------------------\n");
-printw("-                  PROYECTO FINAL                                -\n");
-printw("-                                                                -\n");
-printw("-Creadores:                                                      -\n");
-printw("-*Kalet Adonay Chavez Alvarenga                                  -\n");
-printw("-*Mario Alexander Molina Fuentes                                 -\n");
-printw("-*Adriana Lisbeth Reyes Perez                                    -\n");
-printw("-*Carlos Alejandro Reyes Segovia                                 -\n");
-printw("-*Gladys Alejandra Rivera Iraheta                                -\n");
-printw("-                                                                -\n");
-printw("-Agradecimiento:                                                 -\n");
-printw("-Agradezco a mis compañeros por su dedicacion y trabajo en equipo-\n");
-printw("-Gracias a nuestro catedratico Miguel por su apoyo y colaboración-\n");
-printw("------------------------------------------------------------------\n");
-printw("\n");
-printw("Presiona una tecla para salir. Nos vemos pronto......");
+    clear();
+    
+    printw("------------------------------------------------------------------\n");
+    printw("-                  PROYECTO FINAL                                -\n");
+    printw("-                                                                -\n");
+    printw("-Creadores:                                                      -\n");
+    printw("-*Kalet Adonay Chavez Alvarenga                                  -\n");
+    printw("-*Mario Alexander Molina Fuentes                                 -\n");
+    printw("-*Adriana Lisbeth Reyes Perez                                    -\n");
+    printw("-*Carlos Alejandro Reyes Segovia                                 -\n");
+    printw("-*Gladys Alejandra Rivera Iraheta                                -\n");
+    printw("-                                                                -\n");
+    printw("-Agradecimiento:                                                 -\n");
+    printw("-Agradezco a mis compañeros por su dedicacion y trabajo en equipo-\n");
+    printw("-Gracias a nuestro catedratico Miguel por su apoyo y colaboración-\n");
+    printw("------------------------------------------------------------------\n");
+    printw("\n");
+    printw("Presiona una tecla para salir. Nos vemos pronto......");
 
-refresh();
-
+    refresh();
+    getch();
 }
 
 int main(){
-initscr();
-raw();
-keypad(stdscr, TRUE);
-noecho();
-curs_set(0);
+    initscr();
+    raw();
+    keypad(stdscr, TRUE);
+    noecho();
+    curs_set(0);
 
-int num_lineas = sizeof(creditosfinales) / sizeof(creditosfinales[0]);
+    mostrar_creditos();
 
-int max_y, max_x;
-getmaxyx(stdscr, max_y,max_x);
+    endwin();
 
-start_color();
-init_pair(1, COLOR_WHITE, COLOR_GREEN);
-
-clear();
-
-mostrar_creditos();
-
-for (int i = 0; i < num_lineas && i < max_y; i++) {
-        attron(COLOR_PAIR(1));
-        mvprintw(i,0, %s, creditosfinales[i]);
-        attroff(COLOR_PAIR(1));
-    }
-   
-   refresh(); 
-
-usleep(150000);
-
-getch();
-endwin();
-
-return 0;
+    return 0;
 
 }
